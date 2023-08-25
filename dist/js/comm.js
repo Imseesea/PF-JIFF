34ca4067283fdbfd5891d45469f5457339b72fc2
Imseesea <136055408+Imseesea@users.noreply.github.com>
2023-08-25 18:19:06 +0900

    jiffnews - jiffsns,app

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -27,3 +27,17 @@ var swiper = new Swiper(".swiper-news", {
     hide: false,
   },
 });
+
+var swiper = new Swiper(".swiper-sponsor", {
+  slidesPerView: 7,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+delay: 2500,
+disableOnInteraction: false,
+},
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
